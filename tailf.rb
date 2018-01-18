6a35a065bf4d9e5694d9b1d3b74431e3a4c01f6e
Benoit Liabastre <b.liabastre@gmail.com>
2018-01-18 14:48:19 +0100

    Added indentation for multiline

--- tailf.rb
+++ tailf.rb
@@ -2,19 +2,35 @@
 
 require 'file/tail'
 require 'json'
+require 'terminfo'
+
+LEFT_WIDTH = 42
+LINE_WIDTH = TermInfo.screen_size.last - LEFT_WIDTH
 
 class String
   TO_LENGTH = 30
 
   def to_l(to_length = nil)
-    to_length(self, to_length || TO_LENGTH)
-  end
-
-  def to_length(string, to_length)
-    diff = to_length - length
-    return "#{self}#{diff.times.map{ ' ' }.join}" if diff > 0
+    diff = (to_length || TO_LENGTH) - length
+    return "#{self}#{diff.times.map{' '}.join}" if diff > 0
     self
   end
+
+  def indented
+    parts = self.scan(/.{1,#{LINE_WIDTH}}/)
+    res = parts.shift
+    left_margin = LEFT_WIDTH.times.map{' '}.join
+    parts.each do |part|
+      res = "#{res}\n#{left_margin}#{part}"
+    end
+    res
+  end
+end
+
+def display(json)
+  worker = "[#{(json['worker'] || 'no_worker')}]"
+  status = "[#{(json['status'] || 'info' )}]"
+  puts "#{worker.to_l} #{status.to_l(9)}: #{(json['message'] || '').indented}"
 end
 
 filename = ARGV.pop or fail "Usage: #$0 number filename"
@@ -25,9 +41,6 @@ File::Tail::Logfile.open(filename) do |log|
   log.max_interval = 0.1
   log.backward(number)
   log.tail do |line|
-    json = JSON.parse(line)
-    worker = "[#{(json['worker'] || 'no_worker')}]"
-    status = "[#{(json['status'] || 'info' )}]"
-    puts "#{worker.to_l} #{status.to_l(9)}: #{json['message']}"
+    display(JSON.parse(line))
   end
 end
