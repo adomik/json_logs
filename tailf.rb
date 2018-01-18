e22052eef048045fe2d1d1964db8b0488462fc8c
Benoit Liabastre <b.liabastre@gmail.com>
2018-01-18 12:03:46 +0100

    First commit

---
+++ tailf.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'file/tail'
+require 'json'
+
+class String
+  TO_LENGTH = 30
+
+  def to_l(to_length = nil)
+    to_length(self, to_length || TO_LENGTH)
+  end
+
+  def to_length(string, to_length)
+    diff = to_length - length
+    return "#{self}#{diff.times.map{ ' ' }.join}" if diff > 0
+    self
+  end
+end
+
+filename = ARGV.pop or fail "Usage: #$0 number filename"
+number = (ARGV.pop || 0).to_i.abs
+
+File::Tail::Logfile.open(filename) do |log|
+  log.backward(number).tail do |line|
+    json = JSON.parse(line)
+    worker = "[#{(json['worker'] || 'no_worker')}]"
+    status = "[#{(json['status'] || 'info' )}]"
+    puts "#{worker.to_l} #{status.to_l(9)}: #{json['message']}"
+  end
+end
